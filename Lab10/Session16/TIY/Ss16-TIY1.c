48c554768a07a425042d4f577cb9d275da6406f0
duckun2512 <duckun2512@users.noreply.github.com>
2020-05-24 23:57:54 -0400

    Update Ss16-TIY1.c

--- Lab10/Session16/TIY/Ss16-TIY1.c
+++ Lab10/Session16/TIY/Ss16-TIY1.c
@@ -1,31 +1,32 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+float area(float x) {
+	float area;
+	area = x * x * 3.14;
+	return area;
+}
+
+float perimeter(float y) {
+	float perimeter;
+	perimeter = y * 2 * 3.14;
+	return perimeter;
+}
+
 int main() {
 
-	float r,*pr;
+	float r;
 	float a, b;
 
 	printf("Enter circle's radius: ");
 	scanf("%f", &r);
-
-	pr = &r;
 	
-	a = area(pr);
+	a = area(r);
 	printf("\nArea of circle is: %.2f\n", a);
 
-	b = perimeter(pr);
+	b = perimeter(r);
 	printf("Perimeter of circle is: %.2f\n", b);
+
+	return 1;
 }
 
-area(float *x) {
-	float area;
-	area = (*x) * (*x) * 3.14;
-	return area;
-}
-
-perimeter(float *y) {
-	float perimeter;
-	perimeter = *y * 2 * 3.14;
-	return perimeter;
-}
