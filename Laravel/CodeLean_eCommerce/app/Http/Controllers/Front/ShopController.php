41f8ebfc8a48673c0f40ba6399d9df656d79ecbf
duckun2512 <65001215+duckun2512@users.noreply.github.com>
2021-03-20 12:12:24 -0400

    Project

--- Laravel/CodeLean_eCommerce/app/Http/Controllers/Front/ShopController.php
+++ Laravel/CodeLean_eCommerce/app/Http/Controllers/Front/ShopController.php
@@ -3,15 +3,22 @@
 namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
+use App\Models\Brand;
 use App\Models\Product;
+use App\Models\ProductCategory;
 use App\Models\ProductComment;
 use Faker\Provider\Base;
 use Illuminate\Http\Request;
+use function GuzzleHttp\Psr7\str;
 
 class ShopController extends BaseController
 {
     //
     public function show($id) {
+        //Get categories, brands
+        $categories = ProductCategory::all();
+        $brands = Brand::all();
+
         $product = Product::findOrFail($id);
 
         $avgRating = 0;
@@ -26,7 +33,7 @@ class ShopController extends BaseController
             ->limit(4)
             ->get();
 
-        return view('front.shop.show', compact('product', 'avgRating', 'relatedProduct'));
+        return view('front.shop.show', compact('product', 'avgRating', 'relatedProduct', 'categories', 'brands'));
     }
 
     public function postComment(Request $request) {
@@ -36,8 +43,102 @@ class ShopController extends BaseController
     }
 
     public function index() {
-        $products = Product::paginate(6);
+        //Get categories, brands
+        $categories = ProductCategory::all();
+        $brands = Brand::all();
 
-        return view('front.shop.index', compact('products'));
+        //Get products
+        $perPage = $request->show ?? 3;
+        $sortBy = $request->sort_by ?? 'latest';
+        $search = $request->search ?? '';
+
+        $products = Product::where('name', 'like', '%' . $search . '%');
+
+        $products = $this->sortAndPagination($products, $sortBy, $perPage);
+
+        $products = $this->filter($products, $request);
+
+        return view('front.shop.index', compact('categories', 'brands', 'products'));
+    }
+
+    public function category($categoryName, Request $request) {
+        //Get categories, brands
+        $categories = ProductCategory::all();
+        $brands = Brand::all();
+
+        //Get products
+        $perPage = $request->show ?? 3;
+        $sortBy = $request->sort_by ?? 'latest';
+
+        $products = ProductCategory::where('name', $categoryName)
+            ->first()
+            ->products
+            ->toQuery();
+
+        $products = $this->filter($products, $request);
+
+        $products = $this->sortAndPagination($products, $sortBy, $perPage);
+
+        return view('front.shop.index', compact('categories', 'brands', 'products'));
+    }
+
+    public function sortAndPagination($products, $sortBy, $perPage) {
+
+        switch ($sortBy) {
+            case 'latest':
+                $products = $products->orderBy('id');
+                break;
+            case 'oldest':
+                $products = $products->orderByDesc('id');
+                break;
+            case 'name-ascending':
+                $products = $products->orderBy('name');
+                break;
+            case 'name-descending':
+                $products = $products->orderByDesc('name');
+                break;
+            case 'price-ascending':
+                $products = $products->orderBy('price');
+                break;
+            case 'price-descending':
+                $products = $products->orderByDesc('price');
+                break;
+            default:
+                $products = $products->orderBy('id');
+        }
+
+        $products = $products->paginate($perPage);
+
+        $products->append(['sort_by' => $sortBy, 'show' => $perPage]);
+
+        return $products;
+    }
+
+    public function filter($products, Request $request) {
+        //Brand
+        $brands = $request->brand ?? [];
+        $brand_ids = array_keys($brands);
+        $products = $brand_ids != null ? $products->whereIn('brand_id', $brand_ids) : $products;
+
+        //Price
+        $priceMin = $request->price_min;
+        $priceMax = $request->price_max;
+        $priceMin = str_replace('$', '', $priceMin);
+        $priceMax = str_replace('$', '', $priceMax);
+        $products = ($priceMin != null && $priceMax != null) ? $products->whereBetween('price', [$priceMin, $priceMax]) : $products;
+
+        //Color
+        $color = $request->color;
+        $products = $color != null ? $products->whereHas('productDetails', function ($query) use ($color) {
+            return $query->where('color', $color)->where('qty', '>', 0);
+        }) : $products;
+
+        //Size
+        $size = $request->size;
+        $products = $size != null ? $products->whereHas('productDetails', function ($query) use ($size) {
+            return $query->where('color', $size)    ->where('qty', '>', 0);
+        }) : $products;
+
+        return $products;
     }
 }
--- Laravel/CodeLean_eCommerce/resources/views/front/layout/master.blade.php
+++ Laravel/CodeLean_eCommerce/resources/views/front/layout/master.blade.php
@@ -78,13 +78,15 @@
                     </div>
                 </div>
                 <div class="col-lg-7 col-md-7">
-                    <div class="advanced-search">
-                        <button type="button" class="category-btn">All Categories</button>
-                        <div class="input-group">
-                            <input type="text" placeholder="What do you need?">
-                            <button type="button"><i class="ti-search"></i></button>
+                    <form action="">
+                        <div class="advanced-search">
+                            <button type="button" class="category-btn">All Categories</button>
+                            <div class="input-group">
+                                <input name="search" type="text" value="{{ request('search') }}" placeholder="What do you need?">
+                                <button type="submit"><i class="ti-search"></i></button>
+                            </div>
                         </div>
-                    </div>
+                    </form>
                 </div>
                 <div class="col-lg-3 text-right col-md-3">
                     <ul class="nav-right">
@@ -95,52 +97,42 @@
                             </a>
                         </li>
                         <li class="cart-icon">
-                            <a href="#">
+                            <a href="./cart">
                                 <i class="icon_bag_alt"></i>
-                                <span>3</span>
+                                <span>{{ Cart::count() }}</span>
                             </a>
                             <div class="cart-hover">
                                 <div class="select-items">
                                     <table>
                                         <tbody>
-                                        <tr>
-                                            <td class="si-pic"><img src="img/select-product-1.jpg" alt=""></td>
-                                            <td class="si-text">
-                                                <div class="product-selected">
-                                                    <p>$60.00 x 1</p>
-                                                    <h6>Kabino Bedside Table</h6>
-                                                </div>
-                                            </td>
-                                            <td class="si-close">
-                                                <i class="ti-close"></i>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td class="si-pic"><img src="img/select-product-2.jpg" alt=""></td>
-                                            <td class="si-text">
-                                                <div class="product-selected">
-                                                    <p>$60.00 x 1</p>
-                                                    <h6>Kabino Bedside Table</h6>
-                                                </div>
-                                            </td>
-                                            <td class="si-close">
-                                                <i class="ti-close"></i>
-                                            </td>
-                                        </tr>
+                                            @foreach(Cart::content() as $cart)
+                                                <tr>
+                                                <td class="si-pic"><img style="height: 70px" src="front/img/products/{{ $cart->options->images[0]->path }}" alt=""></td>
+                                                <td class="si-text">
+                                                    <div class="product-selected">
+                                                        <p>${{ number_format($cart->price, 2) }} x {{ $cart->qty }}</p>
+                                                        <h6>{{ $cart->name }}</h6>
+                                                    </div>
+                                                </td>
+                                                <td class="si-close">
+                                                    <i onclick="window.location='./cart/delete/{{ $cart->rowId }}'" class="ti-close"></i>
+                                                </td>
+                                            </tr>
+                                            @endforeach
                                         </tbody>
                                     </table>
                                 </div>
                                 <div class="select-total">
                                     <span>total:</span>
-                                    <h5>$120.00</h5>
+                                    <h5>${{ Cart::total() }}</h5>
                                 </div>
                                 <div class="select-button">
-                                    <a href="shopping-cart.html" class="primary-btn view-card">VIEW CARD</a>
-                                    <a href="check-out.html" class="primary-btn checkout-btn">CHECK OUT</a>
+                                    <a href="./cart" class="primary-btn view-card">VIEW CARD</a>
+                                    <a href="./checkout" class="primary-btn checkout-btn">CHECK OUT</a>
                                 </div>
                             </div>
                         </li>
-                        <li class="cart-price">$150.00</li>
+                        <li class="cart-price">${{ Cart::total() }}</li>
                     </ul>
                 </div>
             </div>
