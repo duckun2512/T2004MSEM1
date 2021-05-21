70d904beb702621a42bfef2fe719004e021dffb8
duckun2512 <65001215+duckun2512@users.noreply.github.com>
2021-05-21 09:36:37 -0400

    Lab

---
+++ UWP/Lab1/Lab s1/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Lab_s1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string email = input1.Text;
+            string header = input2.Text;
+            string content = input3.Text;
+            TxtBlock.Text += email + "\n" + header + "\n" + content + "\n" + "-------------\n";
+        }
+
+        private void input3_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TxtBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
