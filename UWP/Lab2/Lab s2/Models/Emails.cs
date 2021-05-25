763de91237db4480bd26cf026cb51fa31fe1be48
duckun2512 <65001215+duckun2512@users.noreply.github.com>
2021-05-25 09:51:25 -0400

    Lab

---
+++ UWP/Lab2/Lab s2/Models/Emails.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_s2.Models
+{
+    class Emails
+    {
+        private string header;
+        private string email;
+        private string content;
+
+        public Emails(string header, string email, string content)
+        {
+            Header = header;
+            Email = email;
+            Content = content;
+        }
+
+        public string Email // properties
+        {
+            get => email;
+            set => email = value;
+        }
+        public string Header
+        {
+            get => header;
+            set => header = value;
+        }
+
+        public string Content
+        {
+            get => content;
+            set => content = value;
+        }
+    }
+}
