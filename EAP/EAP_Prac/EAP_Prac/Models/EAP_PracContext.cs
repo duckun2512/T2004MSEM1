4b5ab47c546ba9dc362f3e00da3f6000d32755ee
duckun2512 <65001215+duckun2512@users.noreply.github.com>
2021-09-01 09:14:37 -0400

    Update

---
+++ EAP/EAP_Prac/EAP_Prac/Models/EAP_PracContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace EAP_Prac.Models
+{
+    public partial class T2004E_PracticalEAPContext : DbContext
+    {
+        public T2004E_PracticalEAPContext()
+        {
+        }
+
+        public T2004E_PracticalEAPContext(DbContextOptions<T2004E_PracticalEAPContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-OGOG660\MSSQLSERVER09;Initial Catalog=Practical_WAD;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.ToTable("Employee");
+
+                entity.Property(e => e.Email).IsRequired();
+
+                entity.Property(e => e.FirstName).IsRequired();
+
+                entity.Property(e => e.LastName).IsRequired();
+
+                entity.Property(e => e.PhoneNumber).IsRequired();
+
+                entity.Property(e => e.StudentId)
+                    .IsRequired()
+                    .HasColumnName("StudentID");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
